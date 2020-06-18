df5821f54612b507489e2ffd52460cf36e01fd5b
miguel bastidas <m.alejandro_80@hotmail.com>
2020-06-18 12:25:57 -0400

    Updates

---
+++ precache-manifest.55fcdb6395b769818406134883d90598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8347bdb57c954f84554e06349b1e6458",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "64ee85ebb5e8caa8cf6c",
+    "url": "/chat/static/css/main.46bbc42b.chunk.css"
+  },
+  {
+    "revision": "2db7c17e819ae3738216",
+    "url": "/chat/static/js/2.27c7234d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat/static/js/2.27c7234d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ee85ebb5e8caa8cf6c",
+    "url": "/chat/static/js/main.33351895.chunk.js"
+  },
+  {
+    "revision": "5a48685dd2a30a9572fe",
+    "url": "/chat/static/js/runtime-main.a2b42cd7.js"
+  }
+]);
